c1550df6cd2ada6e4690600c571416128b92eeaa
datnt <dat03021999@gmail.com>
2022-10-14 15:26:24 +0700

    init

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ const token = env.parsed.DISCORD_TOKEN;
 // Create a new client instance
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
-    }
-});
-
-
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Ready!');
